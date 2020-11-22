dc1a4034f19149cd52d3c1327bf30ee9f58ba0da
alpden550 <alpden550@gmail.com>
2020-11-22 12:02:59 +0300

    feat(server) add handling urls

--- server.py
+++ server.py
@@ -1,24 +1,36 @@
 import pymorphy2
 from aiohttp import web, ClientSession
 from aiohttp_utils import run
+from anyio import create_task_group
 
 import constants
 from main import get_words, process_article
-from anyio import create_task_group
 
 
 async def index(request: web.Request):
+    query_urls = request.query.get('urls')
+    if not query_urls:
+        return web.json_response(data={"error": "No url founded in the request."}, status=400)
+
     morph = pymorphy2.MorphAnalyzer()
     negative_words = await get_words(constants.NEGATIVE_WORDS)
-    urls = request.query.get('urls')
+
+    urls = query_urls.split(",")
+    if len(urls) > constants.URLS_LIMIT:
+        return web.json_response(data={"error": "too many urls in request, should be 10 or less"}, status=400)
+
+    articles_results = await handle_urls(urls=urls, morph=morph, charged=negative_words)
+
+    return web.json_response(articles_results)
+
+
+async def handle_urls(urls: list, morph: pymorphy2.MorphAnalyzer, charged: list):
     results = []
-
     async with ClientSession() as session:
-        for url in constants.TEST_ARTICLES:
+        for url in urls:
             async with create_task_group() as tg:
-                await tg.spawn(process_article, session, url, negative_words, morph, results)
-
-    return web.json_response(results)
+                await tg.spawn(process_article, session, url, charged, morph, results)
+    return results
 
 
 app = web.Application()
