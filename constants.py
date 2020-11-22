7948c6c06ec6f81e1532e3b7beaeb39afac2b153
alpden550 <alpden550@gmail.com>
2020-11-22 11:16:54 +0300

    feat(contstants) add invalid url in TEST_ARTICLES

--- constants.py
+++ constants.py
@@ -2,6 +2,7 @@ NEGATIVE_WORDS = "charged_dict/negative_words.txt"
 POSITIVE_WORDS = "charged_dict/positive_words.txt"
 
 TEST_ARTICLES = [
+    "http://example.com",
     "https://inosmi.ru/politic/20201119/248572409.html",
     "https://inosmi.ru/social/20201119/248573478.html",
     "https://inosmi.ru/politic/20201119/248573249.html",
